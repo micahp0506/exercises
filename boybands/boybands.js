4d0500c6af8ea6daa1294dd61ae953c0afe62966
micahp0506 <micahp0506@gmail.com>
2015-10-13 10:10:58 -0500

    Added addiitonal functionality in js

--- boybands/boybands.js
+++ boybands/boybands.js
@@ -1,5 +1,7 @@
 var bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One Direction"];
 var vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
+var output = "";
+var output2 = "";
 
 // The number of loops to perform (what if the array changes?)
 var loopCount = 5;
@@ -11,19 +13,25 @@ var currentBand = "";
 var currentVeggie = "";
 
 // Get a reference to the appropriate DOM element for bands
-var bandElement = document.getElementById(???);
+var bandElement = document.getElementById("boy-bands");
 
 // Get a reference to the appropriate DOM element for vegetables
-var veggieElement = ???;
+var veggieElement =document.getElementById("vegetables");
 
 // Start looping
 for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
-
+	
+	
   // Add the band names into the correct <div>
-  currentBand = ???;
+  currentBand = bands[loopTracker];
+  output = output + "<div>" + currentBand + "</div>";
 
 
   // Add the veggie names into the correct <div>
-  currentVeggie = ???;
-
+  currentVeggie = vegetables[loopTracker];
+  output2 = output2 + "<div>" + currentVeggie + "</div>";
 }
+console.log(output);
+console.log(output2);
+bandElement.innerHTML = output;
+veggieElement.innerHTML = output2;
