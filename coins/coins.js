15e3b6d80f18e235e9d19ea3e255ac49a9417dd9
micahp0506 <micahp0506@gmail.com>
2015-10-16 09:53:34 -0500

    finished coins exercise/alot of code/will minimize

--- coins/coins.js
+++ coins/coins.js
@@ -6,19 +6,66 @@ Input: 0.67 Output: { quarters: 2, dimes: 1, nickels: 1, pennies: 2 }
 
 */
 
-function coinCounter () { // Initialize a JavaScript object to hold the coins var coinPurse = {};
+// function coinCounter (money) { // Initialize a JavaScript object to hold the coins var coinPurse = {};
 var coinPurse = {
-	quarter: .25,
-	dime: .10,
-	nickel: .05,
-	penny: .01
+	quarter: 0,
+	dime: 0,
+	nickel: 0,
+	penny: 0
+
 }
 
+// coinPurse.quarters = 0;
 
-var
-coinPurse.quarters = 0;
+// return coinPurse; }
 
-return coinPurse; }
+// var coins = coinCounter() console.log();
+var money = .67
+var input,
+	input2,
+	input3,
+	input4,
+	inout5;
 
-var coins = coinCounter() console.log();
+input = money / .25;
+console.log(input);
+input = Math.floor(input);
+console.log(input);
+coinPurse.quarter = input;
+console.log("quarter(s)", coinPurse.quarter);
+input = input * .25;
+console.log(input);
+
+input2 = money - input;
+console.log(input2);
+input2 = input2 / .10;
+console.log(input2);
+input2 = Math.floor(input2);
+console.log(input2);
+coinPurse.dime = input2;
+console.log("dime(s)", coinPurse.dime);
+input2 = input2 * .10;
+console.log(input2);
+
+input3 = money - input - input2;
+console.log(input3);
+input3 = input3 / .05;
+console.log(input3);
+input3 = Math.floor(input3);
+console.log(input3);
+coinPurse.nickel = input3;
+console.log("nickel(s)", coinPurse.nickel);
+input3 = input3 * .05;
+console.log(input3);
+
+input4 = money - input - input2 - input3;
+console.log(input4);
+input4 = input4 / .01;
+console.log(input4);
+input4 = Math.floor(input4);
+console.log(input4);
+coinPurse.penny = input4;
+console.log("penny(s)", coinPurse.penny);
+input4 = input4 * .01;
+console.log(input4);
 
